978e22b47b9d9303655a06c1a83f71da49842cb8
Marius Nadalin <marius.nadalin@student-cs.fr>
2025-03-11 15:30:43 +0100

    create new branch

--- MAS/agents.py
+++ MAS/agents.py
@@ -1,1 +1,87 @@
-# TODO
+# GROUP : 23
+# DATE : 11.03.2025
+# MEMBERS :
+# - ZUO Yuxian
+# - NADALIN	Marius
+# - EL BARHICHI	Mohammed
+
+import mesa
+from model import RobotMission
+
+class GreenRobot(mesa.Agent):
+    """A robot that lives in the green zone (low radioactivity zone)"""
+
+    def __init__(self, model:RobotMission):
+        """initialize a GreenRobot instance.
+
+        Args:
+            model: A RobotMission instance
+        """
+        super().__init__(model)
+        self.knwoledge = {}
+
+    def percepts(self):
+        # Percieve the surronding environment and update its knowledge
+        pass
+
+    def deliberate(self):
+        # Based on the current knowledge, choose an action to perform
+        pass
+
+    def do(self):
+        # Inform the environment about the chosen action
+        self.percepts()
+        action = self.deliberate()
+        return action
+
+class YellowRobot(mesa.Agent):
+    """A robot that lives in the green & yellow zone (low to medium radioactivity zone)"""
+
+    def __init__(self, model:RobotMission):
+        """initialize a YellowRobot instance.
+
+        Args:
+            model: A RobotMission instance
+        """
+        super().__init__(model)
+        self.knwoledge = {}
+
+    def percepts(self):
+        # Percieve the surronding environment and update its knowledge
+        pass
+
+    def deliberate(self):
+        # Based on the current knowledge, choose an action to perform
+        pass
+
+    def do(self):
+        # Inform the environment about the chosen action
+        self.percepts()
+        action = self.deliberate()
+        return action
+
+class RedRobot(mesa.Agent):
+    """A robot that lives in the green, yellow & red zone (low to high radioactivity zone)"""
+
+    def __init__(self, model:RobotMission):
+        """initialize a RedRobot instance.
+
+        Args:
+            model: A RobotMission instance
+        """
+        super().__init__(model)
+        self.knwoledge = {}
+
+    def percepts(self):
+        # Percieve the surronding environment and update its knowledge
+        pass
+
+    def deliberate(self):
+        # Based on the current knowledge, choose an action to perform
+        pass
+
+    def do(self):
+        # Inform the environment about the chosen action
+        self.percepts()
+        action = self.deliberate()
+        return action
--- MAS/model.py
+++ MAS/model.py
@@ -1,1 +1,71 @@
-# TODO
+# GROUP : 23
+# DATE : 11.03.2025
+# MEMBERS :
+# - ZUO Yuxian
+# - NADALIN	Marius
+# - EL BARHICHI	Mohammed
+
+import mesa
+from agents import GreenRobot, YellowRobot, RedRobot
+
+class RobotMission(mesa.Model):
+    """A model with some number of agents."""
+
+    def __init__(self, n=2, width=10, height=10, seed=None):
+        """Initialize a RobotMission instance.
+    
+        Args:
+            N: The number of agents per radioactivity zone.
+            width: width of the grid.
+            height: Height of the grid.
+        """
+        super().__init__(seed=seed)
+        self.num_agents = n
+        self.grid = mesa.space.MultiGrid(width, height, torus=False)
+        self.zones = {
+            'green' : 
+                {
+                    'bounds' : [(0, width // 3), (0, height)],
+                    'agents' : []
+                },
+            'yellow' : 
+                {
+                    'bounds' : [(width // 3 + 1, 2 * width // 3), (0, height)],
+                    'agents' : []
+                },
+            'red' : 
+                {
+                    'bounds' : [(2 * width // 3 + 1, width), (0, height)],
+                    'agents' : []
+                }
+            }
+
+        # Initialize each zone
+        for zone in self.zones:
+            bounds = self.zones[zone]
+            x_min, x_max = bounds[0]
+            y_min, y_max = bounds[1]
+        
+            # Create agents
+            if zone == 'green':
+                agents = GreenRobot.create_agents(model=self, n=n)
+            elif zone == 'yellow':
+                agents = YellowRobot.create_agents(model=self, n=n)
+            elif zone == 'red':
+                agents = RedRobot.create_agents(model=self, n=n)
+        
+            # Create x and y positions for agents
+            x = self.rng.integers(x_min, x_max, size=(n,))
+            y = self.rng.integers(y_min, y_max, size=(n,))
+            
+            for a, i, j in zip(agents, x, y):
+                # Add the agent to a random grid cell
+                self.grid.place_agent(a, (i, j))
+
+        self.datacollector = mesa.DataCollector()
+        self.datacollector.collect(self)
+
+    def step(self):
+        """do one step of the model"""
+        self.agents.shuffle_do("do")
+        self.datacollector.collect(self)
